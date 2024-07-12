6c946f945f47dbc8b95920e8c963cd13801a29c0
kiyoung92 <go3go1go2@gmail.com>
2024-07-12 17:42:20 +0900

    add: snapshot model 추가

--- prisma/generated/types.ts
+++ prisma/generated/types.ts
@@ -1,13 +1,27 @@
 import type { ColumnType } from 'kysely';
+
 export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   ? ColumnType<S, I | undefined, U>
   : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
+export type snapshot_users = {
+  snapshot_users_id: Generated<number>;
+  id: string;
+  email: string;
+  username: string;
+  password: string | null;
+  about_me: string | null;
+  profile_image_url: string | null;
+  provider: string;
+  created_at: Timestamp;
+  updated_at: Timestamp | null;
+  delete_yn: string | null;
+};
 export type users = {
   id: string;
   email: string;
-  nick_name: string;
+  username: string;
   password: string | null;
   about_me: string | null;
   profile_image_url: string | null;
@@ -17,5 +31,6 @@ export type users = {
   delete_yn: Generated<string | null>;
 };
 export type DB = {
+  snapshot_users: snapshot_users;
   users: users;
 };
